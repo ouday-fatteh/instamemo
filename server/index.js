51670963c528b50bd259a3fc67f915f2786ab9f1
ouday-fatteh <oudayhacker007@gmail.com>
2022-03-30 20:10:50 +0100

    Added dotenv

--- server/index.js
+++ server/index.js
@@ -3,10 +3,11 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import Postroutes from './routes/posts.js';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
-const CONNECTION_URL = "mongodb+srv://ouday11:ouday1999@cluster0.vng4x.mongodb.net/memogram?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000 ;
 
 
@@ -15,6 +16,6 @@ app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use('/api',Postroutes);
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser:true , useUnifiedTopology:true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser:true , useUnifiedTopology:true })
 .then(() => app.listen(PORT , () => console.log(`Server up and running on ${PORT}`)))
 .catch((err) => console.log(err.message));
